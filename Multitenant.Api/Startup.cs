76fab2b49b83d9946b14a5524aa126c876e6cd94
Gleuvanir Albino <gleuvanir@gmail.com>
2022-08-01 17:41:27 +0100

    Inclusão do Swagger

--- Multitenant.Api/Startup.cs
+++ Multitenant.Api/Startup.cs
@@ -37,12 +37,26 @@ namespace Multitenant.Api
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseRouting();
+
             app.UseAuthorization();
-            app.MapRazorPages();
+
+            app.MapControllers();
+
             app.Run();
+            //app.UseHttpsRedirection();
+            //app.UseStaticFiles();
+            //app.UseRouting();
+            //app.UseAuthorization();
+            //app.MapRazorPages();
+            //app.Run();
         }
     }
 }
